ffd6a6b7b8adedcedb8b4af7700645c617503e84
lewisbirks <22620804+lewisbirks@users.noreply.github.com>
2025-05-19 15:30:52 +0100

    logo
    Signed-off-by: lewisbirks <22620804+lewisbirks@users.noreply.github.com>

--- flarc.PortalClient/src/App.tsx
+++ flarc.PortalClient/src/App.tsx
@@ -3,8 +3,17 @@ import {HomeComponent} from './components'
 function App() {
 
   return (
-      <HomeComponent />
-  );
+      <div className="nhsuk-width-container">
+        <main className="nhsuk-main-wrapper" id="maincontent">
+          <div className="nhsuk-grid-row">
+            <div className="nhsuk-grid-column-two-thirds">
+              <HomeComponent/>
+            </div>
+          </div>
+        </main>
+      </div>
+)
+  ;
 }
 
 export default App;
