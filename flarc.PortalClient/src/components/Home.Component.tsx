57935144e7899e9e03bbcc6846b8fe8dd9a275e2
lewisbirks <22620804+lewisbirks@users.noreply.github.com>
2025-05-19 16:16:15 +0100

    just making sure that we use the right things
    Signed-off-by: lewisbirks <22620804+lewisbirks@users.noreply.github.com>

--- flarc.PortalClient/src/components/Home.Component.tsx
+++ flarc.PortalClient/src/components/Home.Component.tsx
@@ -1,9 +1,9 @@
 import './Home.Component.scss'
-import {Label} from "nhsuk-react-components";
+import HeadingLevel from "nhsuk-react-components/dist/esm/components/utils/HeadingLevel";
 
 function HomeComponent() {
     return (
-        <Label isPageHeading={true}>Landing Page</Label>
+        <HeadingLevel className={"nhsuk-heading-l"} headingLevel={"h1"}>Landing Page</HeadingLevel>
     );
 }
 
